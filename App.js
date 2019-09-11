a7a9b529b30fedc1a331ca722a5bcd4d155adfdc
Peter Lu <peterlu@Peters-iMac.local>
2019-09-11 00:14:24 -0400

    change

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button, TouchableHighlight, ScrollView, ListVie
 import Swipeout from 'react-native-swipeout';
 import { Menu, MenuProvider, MenuContext, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
 import { SQLite } from "expo-sqlite";
-import OrderHandler from "./OrderHandler"
+//import OrderHandler from "./OrderHandler"
 //import { Constants } from 'expo';
 
 
--- OrderHandler.js
+++ OrderHandler.js
@@ -3,19 +3,24 @@ import React, { useState, Component } from 'react';
 import { StyleSheet, Text, View, Button, TouchableHighlight, ScrollView, ListView } from 'react-native';
 import Swipeout from 'react-native-swipeout';
 import { Menu, MenuProvider, MenuContext, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
-import { SQLite } from "expo-sqlite";
-import OrderHandler from "./OrderHandler"
+//import { SQLite } from "expo-sqlite";
+
+
 //import { Constants } from 'expo';
 
 
-class OrderHandler {
+function OrderHandler(item) {
 
-    const[text, textAppend] = useState([]);
+    const [text, textAppend] = useState([]);
 
-    AppendItem(item) {
+    function AppendItem(item) {
         textAppend(text => [...text, item]);
     }
 
+    return (<ScrollView style={styles.contentContainer}>
+        {text.map((x) => createOrderItem(x))}
+    </ScrollView>);
+    /*
     static instance = null;
 
     static createInstance() {
@@ -29,4 +34,23 @@ class OrderHandler {
         }
         return mySqlHandler.instance;
     }
+    */
+
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: '#2a4944',
+        borderWidth: 1,
+        backgroundColor: '#d2f7f1',
+        padding: 50
+    },
+    contentContainer: {
+        paddingVertical: 20,
+        fontSize: 18,
+        height: 40
+    }
+});
