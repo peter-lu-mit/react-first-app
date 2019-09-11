336aec09019597a2d7ab1f094f711d35c172e052
Peter Lu <peterlu@Peters-iMac.local>
2019-09-11 00:37:27 -0400

    first commit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState, Component } from 'react';
 import { StyleSheet, Text, View, Button, TouchableHighlight, ScrollView, ListView } from 'react-native';
 import Swipeout from 'react-native-swipeout';
 import { Menu, MenuProvider, MenuContext, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
-import { SQLite } from "expo-sqlite";
+//import { SQLite } from "expo-sqlite";
 //import OrderHandler from "./OrderHandler"
 //import { Constants } from 'expo';
 
